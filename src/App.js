f288d3e3eaa0cf32dba649ba340a1b5e763a9e53
Mudit Arora <mudit.arora0@walmart.com>
2021-10-21 15:03:21 +0530

    implement drag and drop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+const tasks = [
+  {
+    name:"Learn Angular",
+    category:"wip",
+    bgcolor: "yellow"
+  },
+  {
+    name:"React",
+    category:"wip",
+    bgcolor:"pink"
+  },
+  {
+    name:"Vue",
+    category:"complete",
+    bgcolor:"skyblue"
+  }
+]
+
 function App() {
+  const [taskList, setTaskList] = useState(tasks);
+  const tasksByCategory = {wip: [], complete: []};
+  const onDragStart = (e, name) => {
+    e.dataTransfer.setData("name", name);
+  }
+  taskList.forEach(t => {
+    tasksByCategory[t.category].push(
+      <div
+        key={t.name}
+        onDragStart={(e)=> onDragStart(e, t.name)}
+        draggable={true} 
+        style={{backgroundColor: t.bgcolor}}
+        className="draggable"
+      >
+        {t.name}
+      </div>
+    );
+  });
+  const onDragOver =(e) => {
+    e.preventDefault();
+  }
+  const onDrop = (e, category) => {
+    debugger;
+    const name = e.dataTransfer.getData("name");
+    let newTasks = taskList.map(t => {
+      if(t.name === name) {
+        return {...t, category}
+      }
+      return t;
+    });
+    setTaskList(newTasks);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      DRAG & DROP DEMO
+      <div className="tasksContainer">
+        <div className="wip" onDragOver={e => onDragOver(e)} onDrop={e => onDrop(e, "wip")}>
+          <span>WIP</span>
+          {tasksByCategory.wip}
+        </div>
+        <div className="extra"/>
+        <div className="complete" onDragOver={e => onDragOver(e)} onDrop={e => onDrop(e, "complete")}>
+          <span>Complete</span>
+          {tasksByCategory.complete}
+        </div>
+      </div>
     </div>
   );
 }
